8666fa12dada7b551c13a782c32b7f0867cb9b92
thauu-01 <popola957th@gmail.com>
2025-09-20 23:22:25 +0700

    Update code

---
+++ baitap08/src/main/java/baitap08/repository/ProductRepository.java
@@ -0,0 +1,23 @@
+package baitap08.repository;
+
+import baitap08.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findByProductNameContaining(String name);
+
+    Page<Product> findByProductNameContaining(String name, Pageable pageable);
+
+    Optional<Product> findByProductName(String name);
+
+    Optional<Product> findByCreateDate(Date createAt);
+}
